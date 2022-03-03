4a4ee5fbca0235715ea62bb711a7816656f4c10f
rassaoc <rassaoc@gmail.com>
2022-03-03 02:06:42 -0500

    main frame

--- POSmain.py
+++ POSmain.py
@@ -1,2 +1,17 @@
 from tkinter import *
 from tkinter import ttk
+
+class StorePOS:
+
+    def __init__(self, root):
+
+        root.title("General Store POS")
+
+        mainframe = ttk.Frame(root, padding="5")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+root = Tk()
+StorePOS(root)
+root.mainloop()
