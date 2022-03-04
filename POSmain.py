50595394fbdecc477a02325dc3c01baba25b4622
rassaoc <rassaoc@gmail.com>
2022-03-04 04:47:44 -0500

    Adds Styles

--- POSmain.py
+++ POSmain.py
@@ -7,10 +7,17 @@ class StorePOS:
 
         root.title("General Store POS")
 
-        mainframe = ttk.Frame(root, padding="5")
+        s = ttk.Style()
+        s.configure('Frame1.TFrame', background='lightgrey')
+
+        mainframe = ttk.Frame(root, padding="5", style='Frame1.TFrame')
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
+
+        s.configure('Frame2.TFrame', background='grey')
+        checkoutFrame = ttk.Frame(mainframe, padding="5", style='Frame2.TFrame')
+        checkoutFrame.grid(column=4, row=0)
 
 root = Tk()
 StorePOS(root)
